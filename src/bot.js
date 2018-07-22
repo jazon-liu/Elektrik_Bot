d21931c0f19d11e80422095e89e6597dd1d164ad
MagicalTaterTot <shadowbox826@gmail.com>
2018-07-21 20:03:12 -0400

    ez commit

---
+++ src/bot.js
@@ -0,0 +1,16 @@
+const logger = require("winston");
+const Discord = require("discord.js");
+const fs = require("file-system");
+const path = require("path");
+const bot = new Discord.Client();
+
+
+try {
+
+}
+
+client.on('ready', () => {
+  logger.info("Connected");
+});
+
+bot.login(token)
