a44998880994503d848b95413a8518bc172fd8fd
MagicalTaterTot <shadowbox826@gmail.com>
2018-07-22 20:21:08 -0400

    added files for token, api keys, config

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,24 @@
+// Dependencies
+
 const logger = require("winston");
 const Discord = require("discord.js");
 const fs = require("file-system");
 const path = require("path");
+const request = require('request');
+
+// Create bot object
+
 const bot = new Discord.Client();
 
+// Load files
 
-try {
+let tokenFile = JSON.parse(fs.readFileSync(path.join(__dirname, "/../config/token.json")));
+let keyFile = JSON.parse(fs.readFileSync(path.join(__dirname, "/../config/keys.json")));
+let configFile = JSON.parse(fs.readFileSync(path.join(__dirname, "/../config/config.json")));
 
-}
-
-client.on('ready', () => {
-  logger.info("Connected");
+bot.on('ready', async () => {
+  console.log(`MCG Bot online`)
+  bot.user.setActivity("with LCC || " + configFile.prefix + "help");
 });
 
-bot.login(token)
+bot.login(tokenFile.token);
